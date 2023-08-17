5ee499149c83fd94d7be762bb70ca423dbab8c0c
JuanS <sebachodiazp29@gmail.com>
2023-08-17 16:26:15 -0500

    login
    asdasd

--- Trimestre 3/project/chefcitov2/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn = new mysqli("localhost","root","","chefcito");
-    
-    if($conn->connect_errno) 
-    {
-        echo "No hay conexion:(".$conn->connect_errno.")".$conn->connect_error;
-    } 
-?>
-    
