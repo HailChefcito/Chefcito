7bfed9dbfce2b1d56b794b8196fda4d583b2b328
miguel-ballesteros <124916979+miguel-ballesteros@users.noreply.github.com>
2023-08-18 17:05:01 -0500

    Para que el voce voce cambie los nombres

---
+++ Trimestre 3/proyect/chefcitov2/chefcitov2/vistas/admin/eliminarusuario.php
@@ -0,0 +1,7 @@
+<?php
+    include_once("../conex/conexion.php");
+
+    $nd = $_GET['$numeroTelefono1'];
+    mysqli_query($conn, "DELETE  FROM usuarios WHERE numeroTelefono=$nd");
+    header("Location:../admin.php");
+?>
