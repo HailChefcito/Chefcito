888dabab81f69297d3f1df90b7d1cc975ed9c65e
Deiby S <136501261+Deiby101@users.noreply.github.com>
2023-08-22 15:45:47 -0500

    ultimis jiji ji jas

---
+++ chefcitov2/admin/crudadmin.php
@@ -0,0 +1,74 @@
+<?php include_once("conex/conexion.php");?>
+<button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Modidicar Usuario</button>
+
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Buscar Usuario</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <div class="container-fluid">
+    <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="txtbuscar">
+        <button class="btn btn-outline-success" type="submit" name="btnbuscar">Search</button>
+
+    </form>
+</div>
+<?php 
+  if(isset($_POST['btnbuscar']))
+  {
+    $buscar=$_POST['txtbuscar'];
+    $querychefcito=mysqli_query($conn, "SELECT numeroTelefono,nombreUsuario,apellidoUsuario,direccion,emailUsuario,idSexoUsuario,idTipoUsuario,contraseña FROM usuarios WHERE numeroTelefono  LIKE '".$buscar."%'");
+  }else{
+    $querychefcito=mysqli_query($conn, "SELECT numeroTelefono,nombreUsuario,apellidoUsuario,direccion,emailUsuario,idSexoUsuario,idTipoUsuario,contraseña FROM usuarios ORDER BY numeroTelefono  ASC");
+  }
+  while($mostrar = mysqli_fetch_array($querychefcito))
+  {
+       $numeroTelefono  = $mostrar['numeroTelefono'];
+       $nombreUsuario = $mostrar['nombreUsuario'];
+       $apellidoUsuario = $mostrar['apellidoUsuario'];
+       $direccion = $mostrar['direccion'];
+       $emailUsuario = $mostrar['emailUsuario'];
+       $idSexoUsuario  = $mostrar['idSexoUsuario'];
+       $idTipoUsuario  = $mostrar['idTipoUsuario'];
+       $contraseña = $mostrar['contraseña'];
+      }
+        echo"<div class='offcanvas-body'>";
+        echo"<form>";
+        echo"<h1>Usuarios</h1>";
+        echo"<label >N° de telefono</label>";
+        echo"<input type='number' name='phone' value=".$numeroTelefono." readonly> <BR>";
+        echo"<label >Nombres</label>";
+        echo"<input type='text' name='name' value=".$nombreUsuario.">";
+
+        echo"<labe++l >Apellido</labe++l>";
+        echo"<input type='text' name='lstname' value=".$apellidoUsuario.">";
+
+        echo"<label >Tipo de usuario</label><br>";
+        echo"<input  type='radio' name='usuario' value='1' id='cliente' value=".$idTipoUsuario.">";
+        echo"<label  >Cliente</label>";
+        echo"<input  type='radio'  name='usuario' value='2' id='admin'>";
+        echo"<label >Administrador</label><br>";
+
+        echo"<label>Genero</label>
+        <select name='genero' id='gene' value=".$idSexoUsuario." required>
+        <option value='0'>No Seleccionado</option>
+        <option value='1'>Mujer</option>
+        <option value='2'>Hombre</option>
+        </select>";
+
+        echo"<label>Dirrecion</label>";
+        echo"<input type='text' name='address' value=".$direccion."required readonly>";
+        echo"<label >Correo</label>";
+        echo"<input type='text' name='email' value=".$emailUsuario." required readonly>";
+        echo"<label >Contraseña</label><br>";
+        echo"<input type='password' name='password' value=".$contraseña."required readonly><br>";
+
+        echo "<td style='width=26%'><a href=\"admin/modiuser.php?numeroTelefono=$numeroTelefono\">Modificar</a>";
+        echo "<th><a href =\"admin/eliminarusuario.php?numeroTelefono=$numeroTelefono\"onClick=\"return confirm('¿Estas seguro de eliminar a $numeroTelefono?')\">Eliminar</a></th>" ;
+
+          
+      echo"</div>";
+      echo"</div>"; 
+    
+?>
